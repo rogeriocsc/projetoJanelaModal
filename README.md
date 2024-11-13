68cb7ece5279880f48447aa82b384710df18be4b
Rogerio Curtio <rogeriocurtiogithub@gmail.com>
2024-11-13 14:06:52 -0200

    Atualização arquivo index

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# projetoJanelaModal
+Sobre
+___
+Projeto Janela Modal<br>
+<br>
  Projeto janela modal com css html e javaScript
+
+* [x] Html
+* [x] Css
+* [x] javaScript 
+
---
+++ js/script.js
@@ -0,0 +1,9 @@
+let fundoJanel = document.getElementById('fundo-j')
+
+function janelaModal() {
+    fundoJanel.style.display = 'block'
+}
+
+function fecharJanela() {
+    fundoJanel.style.display = 'none'
+}
