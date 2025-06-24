ec829dd231d0b0350ed1c73617c2af8f832005e8
Rogerio Curtio <rogeriocurtiogithub@gmail.com>
2025-06-24 20:22:49 -0300

    Atualização css, html

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
-let fundoJanela = document.getElementById('fundo')
-let janelaModal = document.getElementById('jan')
+let fundoJanela = window.document.getElementById('janel')
+let janela = window.document.getElementById('jan')
 
 function abrirJanela() {
     fundoJanela.style.display = 'block'
-    janelaModal.style.display = 'block'
+    janela.style.display = 'block'
 }
 
 function fecharJanela() {
     fundoJanela.style.display = 'none'
-    janelaModal.style.display = 'none'
+    janela.style.display = 'none'
 }
